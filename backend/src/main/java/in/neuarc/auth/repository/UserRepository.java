556e7a721dd3408b77e299e5b500721119c18a49
ganesh714 <evvganesh1@gmail.com>
2026-06-02 11:49:17 +0530

    created user repo

---
+++ backend/src/main/java/in/neuarc/auth/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package in.neuarc.auth.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.neuarc.auth.model.User;
+
+public interface UserRepository extends JpaRepository<User, UUID>{
+	
+	Optional<User> findByEmail(String email);
+	
+	boolean existsByEmail(String email);
+	
+	Optional<User> findByGoogleId(String googleId);
+}
