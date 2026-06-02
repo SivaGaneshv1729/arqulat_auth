31850a8337968b27f22bc78ad2e29147fa95d35a
ganesh714 <evvganesh1@gmail.com>
2026-06-02 11:49:08 +0530

    created user model

---
+++ backend/src/main/java/in/neuarc/auth/model/User.java
@@ -0,0 +1,42 @@
+package in.neuarc.auth.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(updatable = false,nullable = false)
+	private UUID id;
+	
+	@Column(unique = true,nullable = false)
+	private String email;
+	
+	@Column(name = "password_hash")
+	private String passwordHash;
+	
+	@Column(name = "google_id",unique = true)
+	private String googleId;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+}
